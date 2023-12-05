80daacbe8d2085f008edf17a535839e2c0a9c0a8
Marc Brgs <marc.brgs@yahoo.fr>
2023-12-05 14:11:05 +0100

    Use Fibonacci dll in Unity

--- Machine-Learning/Assets/Scripts/TestML.cs
+++ Machine-Learning/Assets/Scripts/TestML.cs
@@ -1,13 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Runtime.InteropServices;
 
 public class TestML : MonoBehaviour
 {
+    [DllImport("MLLibrary")]
+    public static extern void fibonacci_init(ulong a, ulong b);
+
+    [DllImport("MLLibrary")]
+    public static extern bool fibonacci_next();
+
+    [DllImport("MLLibrary")]
+    public static extern ulong fibonacci_current();
+
+    [DllImport("MLLibrary")]
+    public static extern ulong fibonacci_index();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Initialize a Fibonacci relation sequence.
+        fibonacci_init(1, 1);
+        // Write out the sequence values until overflow.
+        do
+        {
+            Debug.Log(fibonacci_index() + ": " + fibonacci_current());
+        } while (fibonacci_next());
+        // Report count of values written before overflow.
+        Debug.Log(fibonacci_index() + 1 + " Fibonacci sequence values fit in an unsigned 64-bit integer.");
     }
 
     // Update is called once per frame
