7234f80954fd0220a4a52ef54d788948a89b5858
Marc Brgs <marc.brgs@yahoo.fr>
2024-01-11 16:56:16 +0100

    Load image pixels components (RGB)

--- Machine-Learning/Assets/Scripts/App.cs
+++ Machine-Learning/Assets/Scripts/App.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class App : MonoBehaviour
@@ -17,6 +18,9 @@ public class App : MonoBehaviour
     {
         p = new PerceptronWrapper(2, new int[] { 2 }, 1);
         p.learningRate = 0.05f;
+
+        Texture2D tex = LoadPNG("./Assets/Dataset/Train/Rocket League/RL1Image-000.jpg");
+        PixelLoader(tex);
     }
 
     // Update is called once per frame
@@ -28,11 +32,11 @@ public class App : MonoBehaviour
         {
             if (epoch < epochs)
             {
-                for(int i = 0; i < 100; i++) {
-                    p.train(new double[] { 0, 0 }, new double[] { 0 });
-                    p.train(new double[] { 0, 1 }, new double[] { 1 });
-                    p.train(new double[] { 1, 0 }, new double[] { 1 });
-                    p.train(new double[] { 1, 1 }, new double[] { 0 });
+                for(int i = 0; i < epochCluster; i++) {
+                    //p.train(new double[] { 0, 0 }, new double[] { 0 });
+                    //p.train(new double[] { 0, 1 }, new double[] { 1 });
+                    //p.train(new double[] { 1, 0 }, new double[] { 1 });
+                    //p.train(new double[] { 1, 1 }, new double[] { 0 });
                     epoch++;
                 }
 
@@ -75,5 +79,39 @@ public class App : MonoBehaviour
                 Debug.Log("Training resumed");
             }
         }
+    }
+
+    public static Texture2D LoadPNG(string filePath)
+    {
+
+        Texture2D tex = null;
+        byte[] fileData;
+
+        if (File.Exists(filePath))
+        {
+            fileData = File.ReadAllBytes(filePath);
+            tex = new Texture2D(48, 27); // 48x27 pixels (1296 pixels)
+            tex.LoadImage(fileData); //..this will auto-resize the texture dimensions
+        }
+        return tex;
+    }
+
+    public double[] PixelLoader(Texture2D tex)
+    {
+        List<double> myList = new List<double>();
+        for (int i = 0; i < tex.width; i++)
+        {
+            for (int j = 0; j < tex.height; j++)
+            {
+                Color pixel = tex.GetPixel(i, j);
+
+                myList.Add(pixel.r);
+                myList.Add(pixel.g);
+                myList.Add(pixel.b);
+
+            }
+        }
+        Debug.Log(myList.Count);
+        return myList.ToArray();
     }
 }
